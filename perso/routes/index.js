755b88358833411f451aa5f3ba131c7fe82121ec
XENOR31000 <axel.ibanez2@lycee-pardailhan.fr>
2023-01-11 21:47:59 +0100

    test

--- perso/routes/index.js
+++ perso/routes/index.js
@@ -1,10 +1,9 @@
 import express from "express";
-import recipesRoutes from "./recipes";
+
 import usersRoutes from "./users";
 
 const router = express.Router();
 
-router.use(recipesRoutes);
 
 router.use(usersRoutes);
 
